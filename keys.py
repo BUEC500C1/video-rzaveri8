51bfe4c18d9d7388194469c1da7a08ff890d03c2
Ruby <rzaveri@bu.edu>
2020-02-28 17:16:51 -0500

    clean-up

--- keys.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "Vq50sTNMsLfgdfHLqEcssZuEr"
-consumer_secret = "zHB87pdpWg0ZFSgx5MgwwYbHWjg4C5w73aM7XPvri5PuEql8L8"
-access_token = "1227368599725080577-LJNZkmkIpFUOkj2wEZ7bCMR0s5403g"
-access_secret = "NqFpeZpY5bM0Bra4WInduDHqOSdWTiXDXzqYFHPXIsk28"
